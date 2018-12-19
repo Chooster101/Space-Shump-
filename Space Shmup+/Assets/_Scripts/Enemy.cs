72896d0fe1a0c5b30928bc66f6907a6fcb51cbdc
Chooster101 <chooster101@gmail.com>
2018-12-19 03:18:40 -0500

    Space Shump+ Prototype
    Finished Prototype. has BoundsCheck error.

--- Space Shmup+/Assets/_Scripts/Enemy.cs
+++ Space Shmup+/Assets/_Scripts/Enemy.cs
@@ -9,11 +9,28 @@ public class Enemy : MonoBehaviour {
     public float fireRate = 0.3f; // Seconds/shot (Unused)
     public float health = 10;
     public int score = 100; // Points earned for destroying this
+    public float showDamageDuration = 0.1f; // # seconds to show damage 
+    public float powerUpDropChance = 1f;  // Chance to drop a power-up 
 
-    private BoundsCheck bndCheck;
+    [Header("Set Dynamically: Enemy")]
+    public Color[] originalColors;
+    public Material[] materials;// All the Materials of this & its children
+    public bool showingDamage = false;
+    public float damageDoneTime; // Time to stop showing damage
+    public bool notifiedOfDestruction = false; // Will be used later
+
+    protected BoundsCheck bndCheck;
+
     void Awake()
     { 
         bndCheck = GetComponent<BoundsCheck>();
+        // Get materials and colors for this GameObject and its children
+        materials = Utils.GetAllMaterials(gameObject);     
+        originalColors = new Color[materials.Length];
+        for (int i = 0; i < materials.Length; i++)
+        {
+            originalColors[i] = materials[i].color;
+        }
     }
 
     // This is a Property: A method that acts like a field
@@ -32,6 +49,11 @@ public class Enemy : MonoBehaviour {
     void Update()
     {
         Move();
+
+        if (showingDamage && Time.time > damageDoneTime)
+        { 
+            UnShowDamage();
+        }
 
         if (bndCheck != null && bndCheck.offDown)
         { 
@@ -52,17 +74,59 @@ public class Enemy : MonoBehaviour {
     }
 
     void OnCollisionEnter(Collision coll)
-    {
-        GameObject otherGO = coll.gameObject; 
-        if (otherGO.tag == "ProjectileHero")
-        { 
-            Destroy(otherGO); // Destroy the Projectile
-            Destroy(gameObject); // Destroy this Enemy GameObject
-        }
-        else
+    {                 
+        GameObject otherGO = coll.gameObject;
+        switch (otherGO.tag)
         {
-            print("Enemy hit by non-ProjectileHero: " + otherGO.name); 
+            case "ProjectileHero":                               
+                Projectile p = otherGO.GetComponent<Projectile>();
+                // If this Enemy is off screen, don't damage it.
+                if (!bndCheck.isOnScreen)
+                {                     
+                    Destroy(otherGO);
+                    break;
+                }
+                // Hurt this Enemy
+                ShowDamage();
+                // Get the damage amount from the Main WEAP_DICT.
+                health -= Main.GetWeaponDefinition(p.type).damageOnHit;
+                if (health <= 0)
+                {
+                    // Tell the Main singleton that this ship was destroyed 
+                    if (!notifiedOfDestruction)
+                    {
+                        Main.S.ShipDestroyed(this);
+                    }
+                    notifiedOfDestruction = true;
+                    // Destroy this Enemy
+                    Destroy(this.gameObject);
+                }
+                Destroy(otherGO);                   
+                break;
+            default:
+                print("Enemy hit by non-ProjectileHero: " + otherGO.name); 
+                break;
         }
     }
+
+    void ShowDamage()
+    { 
+        foreach (Material m in materials)
+        {
+            m.color = Color.red;
+        }
+        showingDamage = true;
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+
+    void UnShowDamage()
+    { 
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = originalColors[i];
+        }
+        showingDamage = false;
+    }
+
 }
 
--- Space Shmup+/Assets/_Scripts/Main.cs
+++ Space Shmup+/Assets/_Scripts/Main.cs
@@ -5,30 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
     static public Main S; // A singleton for Main
+    static Dictionary<WeaponType, WeaponDefinition> WEAP_DICT;
+
 
     [Header("Set in Inspector")]
     public GameObject[] prefabEnemies; // Array of Enemy prefabs
     public float enemySpawnPerSecond = 0.5f; // # Enemies/second
     public float enemyDefaultPadding = 1.5f; // Padding for position
+    public WeaponDefinition[] weaponDefinitions;
+    public GameObject prefabPowerUp; 
+    public WeaponType[] powerUpFrequency = new WeaponType[] { 
+                                    WeaponType.blaster, WeaponType.blaster,
+                                    WeaponType.spread,  WeaponType.shield };
+
     private BoundsCheck bndCheck;
 
+    public void ShipDestroyed(Enemy e)
+    {
+        // Potentially generate a PowerUp
+        if (Random.value <= e.powerUpDropChance)
+        {
+            // Choose which PowerUp to pick
+            // Pick one from the possibilities in powerUpFrequency
+            int ndx = Random.Range(0, powerUpFrequency.Length);
+            WeaponType puType = powerUpFrequency[ndx];
+            // Spawn a PowerUp
+            GameObject go = Instantiate(prefabPowerUp) as GameObject;
+            PowerUp pu = go.GetComponent<PowerUp>();
+            // Set it to the proper WeaponType
+            pu.SetType(puType);
+            // Set it to the position of the destroyed ship
+            pu.transform.position = e.transform.position;
+        }
+    }
     void Awake()
     {
         S = this;
         // Set bndCheck to reference the BoundsCheck component on this GameObject
         bndCheck = GetComponent<BoundsCheck>();
         Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+        WEAP_DICT = new Dictionary<WeaponType, WeaponDefinition>();     
+        foreach (WeaponDefinition def in weaponDefinitions)
+        {          
+            WEAP_DICT[def.type] = def;
+        }
     }
-    
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void SpawnEnemy()
     {
@@ -62,5 +83,27 @@ public class Main : MonoBehaviour {
     {
         // Reload _Scene_0 to restart the game
         SceneManager.LoadScene("SampleScene");
+    }
+
+    /// <summary>
+    /// Static function that gets a WeaponDefinition from the WEAP_DICT static
+    ///   protected field of the Main class.
+    /// </summary>
+    /// <returns>The WeaponDefinition or, if there is no WeaponDefinition with
+    ///   the WeaponType passed in, returns a new WeaponDefinition with a
+    ///   WeaponType of none..</returns>
+    /// <param name="wt">The WeaponType of the desired WeaponDefinition</param>
+    static public WeaponDefinition GetWeaponDefinition(WeaponType wt)
+    {   // a
+        // Check to make sure that the key exists in the Dictionary
+        // Attempting to retrieve a key that didn't exist, would throw an error,
+        //   so the following if statement is important.
+        if (WEAP_DICT.ContainsKey(wt))
+        {                                     
+            return (WEAP_DICT[wt]);
+        }
+        // This returns a new WeaponDefinition with a type of WeaponType.none,
+        //   which means it has failed to find the right WeaponDefinition
+        return (new WeaponDefinition());                                   
     }
 }
